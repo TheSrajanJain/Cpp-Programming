9157d49d09b75ee48e1911401749f733ff0d8a8a
Srajan Jain <thesrajanjain@gmail.com>
2025-06-03 22:09:24 +0530

    Create FactorialRecursive.cpp

---
+++ FactorialRecursive.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n <= 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int number;
+
+    cout << "Factorial Calculator (using recursion)\n";
+    cout << "Enter a positive integer: ";
+    cin >> number;
+
+    if (number < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        cout << "Factorial of " << number << " = " << factorial(number) << endl;
+    }
+
+    return 0;
+}
