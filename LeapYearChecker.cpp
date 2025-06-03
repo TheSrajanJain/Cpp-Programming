49688804d99dcb6a56a646ea7578442cc2a53146
Srajan Jain <thesrajanjain@gmail.com>
2025-06-03 22:04:24 +0530

    Create LeapYearChecker.cpp

---
+++ LeapYearChecker.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+
+    cout << "Leap Year Checker\n";
+    cout << "Enter a year: ";
+    cin >> year;
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+                cout << year << " is a Leap Year." << endl;
+            else
+                cout << year << " is NOT a Leap Year." << endl;
+        } else {
+            cout << year << " is a Leap Year." << endl;
+        }
+    } else {
+        cout << year << " is NOT a Leap Year." << endl;
+    }
+
+    return 0;
+}
